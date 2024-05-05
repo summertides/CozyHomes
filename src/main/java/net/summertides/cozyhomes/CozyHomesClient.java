f09d6b23337533a9cb6da911ded9384f2ad4564a
summertides <fdfgehge@gmail.com>
2024-05-05 02:32:19 -0400

    Initial Workspace

---
+++ src/main/java/net/summertides/cozyhomes/CozyHomesClient.java
@@ -0,0 +1,12 @@
+package net.summertides.cozyhomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozyHomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
+
